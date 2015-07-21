2b6de8f94e0cfb4d229d18ef721109892185db73
mikelim098098 <mikelim098098@gmail.com>
2015-07-21 14:02:12 -0700

    trying to push my main controller

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,12 +5,68 @@ class Main extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->output->enable_profiler();
+		// $this->output->enable_profiler();
 	}
 
 	public function index()
 	{
 		echo "Welcome to CodeIgniter. The default Controller is Main.php";
+	}
+
+	public function user_profile()
+	{
+		$this->load->view('user_profile');
+	}
+
+	public function user_orders_this_week()
+	{
+		$this->load->view('user_orders_this_week');
+	}
+
+	public function user_orders_this_month()
+	{
+		$this->load->view('user_orders_this_month');
+	}
+
+	public function user_orders_this_lifetime()
+	{
+		$this->load->view('user_orders_this_lifetime');
+	}
+
+
+	public function user_reviews_this_week()
+	{
+		$this->load->view('user_reviews_this_week');
+	}
+	public function user_reviews_this_month()
+	{
+		$this->load->view('user_reviews_this_month');
+	}
+	public function user_reviews_this_lifetime()
+	{
+		$this->load->view('user_reviews_this_lifetime');
+	}
+
+
+
+	public function chef_profile()
+	{
+		$this->load->view('chef_profile');
+	}
+
+	public function chef_menu()
+	{
+		$this->load->view('chef_menu');
+	}
+
+	public function chef_orders_this_week()
+	{
+		$this->load->view('chef_orders_this_week');
+	}
+
+	public function chef_reviews_this_week()
+	{
+		$this->load->view('chef_reviews_this_week');
 	}
 }
 
